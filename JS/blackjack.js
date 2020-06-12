a963d740d717a278f4c25ea32a632e3e50b5d895
Travis CI <najwer23@gmail.com>
2020-06-12 21:55:24 +0200

    deck as Array Json

--- JS/blackjack.js
+++ JS/blackjack.js
@@ -1,3 +1,4 @@
+
 class Card {
     constructor(params) {
        
@@ -5,9 +6,90 @@ class Card {
 }
 
 window.onload = function () {
-    
-    let deck = new Array();
-    console.log("hello")
+    initOnLoad();  
+}
 
+function initOnLoad() {
+    let deckArrJson = [].concat(
+        load13JsonCards("Clubs"),
+        load13JsonCards("Diamonds"),
+        load13JsonCards("Hearts"),
+        load13JsonCards("Spades"),
+    )
+
+    let deckArrObj = new Array();
+    console.log(cardsJson)
+}
+
+function load13JsonCards(suit) {
+    let cardsJson = [
+        {
+            "suit": suit,
+            "name": "Ace",
+            "value": 1 
+        },
+        {
+            "suit": suit,
+            "name": "2",
+            "value": 2   
+        },
+        {
+            "suit": suit,
+            "name": "3",
+            "value": 3   
+        },
+        {
+            "suit": suit,
+            "name": "4",
+            "value": 4  
+        },
+        {
+            "suit": suit,
+            "name": "5",
+            "value": 5  
+        },
+        {
+            "suit": suit,
+            "name": "6",
+            "value": 6  
+        },
+        {
+            "suit": suit,
+            "name": "7",
+            "value": 7  
+        },
+        {
+            "suit": suit,
+            "name": "8",
+            "value": 8  
+        },
+        {
+            "suit": suit,
+            "name": "9",
+            "value": 9  
+        },
+        {
+            "suit": suit,
+            "name": "10",
+            "value": 10  
+        },
+        {
+            "suit": suit,
+            "name": "Jack",
+            "value": 10 
+        },
+        {
+            "suit": suit,
+            "name": "Queen",
+            "value": 10 
+        },
+        {
+            "suit": suit,
+            "name": "King",
+            "value": 10 
+        },        
+    ]
+
+    return cardsJson;
 }
 
